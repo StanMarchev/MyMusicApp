ca095d41fe3443b11629c6ec07e5953e73b598b8
Stanimir Marchev <114108086+StanMarchev@users.noreply.github.com>
2023-06-21 18:39:08 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "music",
         "USER": "postgres",
-        "PASSWORD": "bgiknow123",
+        "PASSWORD": "*******",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
